3ba1b2e1d9084fb8fe8d8510723cc36bf8ba3293
Gilbert Ndollane Dione <gilbertndollanedione@Gilberts-MacBook-Pro.local>
2024-06-05 09:39:59 +0400

    Remove unneeded 'return'

--- p24/src/refs.rs
+++ p24/src/refs.rs
@@ -1,30 +1,16 @@
 pub fn f1(s: &mut (u32, u32), flag: bool) -> &mut u32 {
     match flag {
-        false => return &mut s.0,
-        true => return &mut s.1,
+        false => &mut s.0,
+        true => &mut s.1,
     }
 }
 
-// pub fn f2(s: &mut [u32], length: usize) -> &u32 {
-//     let x: &mut [u32];
-//     x = &mut s[length - 1..3];
-//     return &x[1];
-// }
-
-// pub fn f2(s: &mut [u32], length: usize) -> &u32 {
-//     return &mut s[length - 1];
-// }
-
-// pub fn f3(s: &mut [u32], length: usize) -> &u32 {
-//     return &mut s[s.len() - length];
-// }
-
 pub fn f2(s: &mut [u32], length: usize) -> &mut u32 {
-    return &mut s[length - 1];
+    &mut s[length - 1]
 }
 
 pub fn f3(s: &mut [u32], length: usize) -> &mut u32 {
-    return &mut s[s.len() - length];
+    &mut s[s.len() - length]
 }
 
 pub fn f4(s: &mut [u32]) -> Vec<[u32; 4]> {
@@ -35,5 +21,5 @@ pub fn f4(s: &mut [u32]) -> Vec<[u32; 4]> {
         intermediate_slice.clone_from_slice(&s[4 * i..4 * (i + 1)]);
         vector_of_slices.push(intermediate_slice);
     }
-    return vector_of_slices;
+    vector_of_slices
 }
