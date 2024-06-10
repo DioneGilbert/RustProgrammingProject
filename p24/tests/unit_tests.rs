9c0e743eecb34f924ba68d0bb2a314b8805c21ea
Gilbert Ndollane Dione <gilbertndollanedione@Gilberts-MacBook-Pro.local>
2024-06-10 21:22:44 +0400

    Fix tests

--- p24/tests/unit_tests.rs
+++ p24/tests/unit_tests.rs
@@ -9,23 +9,28 @@ pub fn test_f1() {
 #[test]
 pub fn test_f2() {
     let mut s_f1: [u32; 6] = [23, 25, 19, 20, 30, 40];
-    assert_eq!(&mut 30, p24::refs::f2(&mut s_f1, 5));
-    assert_eq!(&mut 19, p24::refs::f2(&mut s_f1, 3));
+    assert_eq!(&mut 40, p24::refs::f2(&mut s_f1, 5));
+    assert_eq!(&mut 23, p24::refs::f2(&mut s_f1, 0));
+    assert_eq!(&mut 20, p24::refs::f2(&mut s_f1, 3));
 }
 
 #[test]
 pub fn test_f3() {
     let mut s_f1: [u32; 6] = [23, 25, 19, 20, 30, 40];
-    assert_eq!(&mut 25, p24::refs::f3(&mut s_f1, 5));
-    assert_eq!(&mut 20, p24::refs::f3(&mut s_f1, 3));
+    assert_eq!(&mut 23, p24::refs::f3(&mut s_f1, 5));
+    assert_eq!(&mut 40, p24::refs::f3(&mut s_f1, 0));
+    assert_eq!(&mut 19, p24::refs::f3(&mut s_f1, 3));
 }
 
 #[test]
 pub fn test_f4() {
-    let mut s_f1: [u32; 6] = [23, 25, 19, 20, 30, 40];
-    assert_eq!(&mut 20, p24::refs::f3(&mut s_f1, 3));
+    let mut s_f4: [u32; 13] = [23, 25, 19, 20, 30, 40, 15, 8, 18, 17, 11, 14, 50];
 
-    let mut mysl: [u32; 10] = [23, 25, 19, 20, 30, 40, 50, 70, 45, 80];
-    let test_vec: Vec<[u32; 4]> = vec![[23, 25, 19, 20], [30, 40, 50, 70]];
-    assert_eq!(test_vec, p24::refs::f4(&mut mysl));
+    let test_slice_f4: [&[u32]; 4] = [
+        &[23, 25, 19],
+        &[20, 30, 40],
+        &[15, 8, 18],
+        &[17, 11, 14, 50],
+    ];
+    assert_eq!(test_slice_f4, p24::refs::f4(&mut s_f4));
 }
